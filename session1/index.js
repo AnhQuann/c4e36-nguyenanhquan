7d51a064412fa901efe26a48604ff92e8d4c67ad
AnhQuann <anhquan19051905@gmail.com>
2019-06-17 21:57:41 +0700

    abcxyz

---
+++ session1/index.js
@@ -0,0 +1,27 @@
+// day la doan code cong hai so
+
+// const x = Number(prompt("Enter X"));
+// const y = Number(prompt("Enter Y"));
+
+// console.log(x+y);
+
+// loop
+
+// comment code
+
+// scope
+for(let i = 1; i < 101; i++) {
+  console.log(i);
+}
+
+for(let i = 1; i < 101; i+=5) {
+  console.log(i);
+}
+
+for(let i = 100; i > 0; i--) {
+  console.log(i);
+}
+
+for(let i = 100; i > 0; i-=3) {
+  console.log(i);
+}
